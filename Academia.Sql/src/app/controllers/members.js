2326680c0e882beb033777d7eafd39fc497fb942
Felipe Augusto Tiburcio Viana <tiburcioviana@Felipes-MacBook-Pro.local>
2020-07-26 14:01:05 -0700

    Academia.Sql

---
+++ Academia.Sql/src/app/controllers/members.js
@@ -0,0 +1,110 @@
+const { age, date } = require('../lib/utils')
+
+const Member = require('../models/member')
+
+module.exports = {
+    index(req, res) {
+
+        let { filter, page, limit } = req.query
+
+        page = page || 1
+        limit = limit || 2
+
+        let offset = limit * (page - 1)
+
+        const params = {
+            filter,
+            page,
+            limit,
+            offset,
+            callback(members){
+
+                const pagination = {
+                    total: Math.ceil(members[0].total / limit),
+                    page    
+                }
+                return res.render("members/index", { members, pagination , filter })
+
+            }
+        }
+        
+        Member.paginate(params)
+
+    },
+
+    create(req, res) {
+
+        Member.instructorsSelectOptions(function(options){
+            return res.render('members/create', { instructorOptions: options })
+        })
+    },
+
+    post(req, res) {
+        //req.query.id
+        //Validacao
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields!')
+            }
+        }
+        
+        Member.create(req.body, function(member){
+            return res.redirect(`/members/${member.id}`)
+        })
+
+        //Organizando os dados que quero enviar
+        let { avatar_url, birth, name, services, gender } = req.body
+
+    },
+
+    show(req, res) {
+
+        Member.find(req.params.id, function(member){
+            if(!member) return res.send("Instructor not found")
+
+            member.birth = date(member.birth).bithDay
+
+            return res.render("members/show", { member })
+        })
+
+    },
+
+    edit(req, res) {
+        Member.find(req.params.id, function(member){
+            if(!member) return res.send("Instructor not found")
+
+            member.birth = date(member.birth).iso
+
+            Member.instructorsSelectOptions( function(options){
+                return res.render('members/edit', { member, instructorOptions:options })
+            })
+        })
+    },
+
+    put(req, res) {
+        //req.query.id
+        //Validacao
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields!')
+            }
+        }
+     
+
+        Member.update(req.body, function(){
+            return res.redirect(`members/${req.body.id}`)
+        })
+
+    },
+
+    delete(req, res) {
+      
+        Member.delete(req.body.id, function(){
+            return res.redirect(`members`)
+        })
+    },
+}
